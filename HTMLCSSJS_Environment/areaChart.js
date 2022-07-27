0d2ff6a6b2fc87290f9c732f0eb785ce34256aa4
Connor <connoreaster@gmail.com>
2022-07-27 20:14:41 +0100

    Stack line chart

--- HTMLCSSJS_Environment/areaChart.js
+++ HTMLCSSJS_Environment/areaChart.js
@@ -1,17 +1,66 @@
-// line chart data
-var buyerData = {
-    labels : ["January","February","March","April","May","June"],
-    datasets : [
-    {
-        fillColor : "rgba(172,194,132,0.4)",
-        strokeColor : "#ACC26D",
-        pointColor : "#fff",
-        pointStrokeColor : "#9DB86D",
-        data : [203,156,99,251,305,247]
+new Chart(document.getElementById("myChart"), {
+    type: 'line',
+    data: {
+        labels: [1500, 1600, 1700],
+        datasets: [{
+            data: [100, 300, 200],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: [200, 400, 300],
+            label: "Asia",
+            borderColor: "#8e5ea2",
+            fill: false
+        }, {
+            data: [300, 200, 400],
+            label: "Europe",
+            borderColor: "#3cba9f",
+            fill: false
+        }, {
+            data: [1000, 500, 500],
+            label: "Latin America",
+            borderColor: "#e8c3b9",
+            fill: false
+        }, {
+            data: [500, 100, 100],
+            label: "North America",
+            borderColor: "#c45850",
+            fill: false
+        }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart - stacked=true'
+            },
+            tooltip: {
+                mode: 'index'
+            },
+        },
+        interaction: {
+            mode: 'nearest',
+            axis: 'x',
+            intersect: false
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Month'
+                }
+            },
+            y: {
+                stacked: true,
+                title: {
+                    display: true,
+                    text: 'Value'
+                }
+            }
+        }
     }
-]
-}
-// get line chart canvas
-var buyers = document.getElementById('buyers').getContext('2d');
-// draw line chart
-new Chart(buyers).Line(buyerData);
+});
+
