c336342d0e240e29c3720d5fcac9acd7a0de44fd
Connor <connoreaster@gmail.com>
2022-07-30 17:09:47 +0100

    Chart formatting

--- HTMLCSSJS_Environment/areaChart.js
+++ HTMLCSSJS_Environment/areaChart.js
@@ -1,32 +1,25 @@
 new Chart(document.getElementById("myChart"), {
     type: 'line',
     data: {
-        labels: [1500, 1600, 1700],
+        labels: ['June', 'July', 'August'],
         datasets: [{
             data: [100, 300, 200],
             label: "Africa",
-            borderColor: "#3e95cd",
-            fill: false
+            borderColor: "#81b6cc",
+            backgroundColor: "#81b6cc",
+            fill: true
         }, {
             data: [200, 400, 300],
             label: "Asia",
-            borderColor: "#8e5ea2",
-            fill: false
+            borderColor: "#81cc9e",
+            backgroundColor: "#81cc9e",
+            fill: true
         }, {
             data: [300, 200, 400],
             label: "Europe",
-            borderColor: "#3cba9f",
-            fill: false
-        }, {
-            data: [1000, 500, 500],
-            label: "Latin America",
-            borderColor: "#e8c3b9",
-            fill: false
-        }, {
-            data: [500, 100, 100],
-            label: "North America",
-            borderColor: "#c45850",
-            fill: false
+            borderColor: "#9081cc",
+            backgroundColor: "#9081cc",
+            fill: true
         }
         ]
     },
@@ -35,7 +28,7 @@ new Chart(document.getElementById("myChart"), {
         plugins: {
             title: {
                 display: true,
-                text: 'Chart.js Line Chart - stacked=true'
+                text: 'Financial Chart'
             },
             tooltip: {
                 mode: 'index'
