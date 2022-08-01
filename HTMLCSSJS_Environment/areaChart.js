727769a636170878e34bcedc57e8ab66a88f17c3
Connor <connoreaster@gmail.com>
2022-08-01 14:35:40 +0100

    chart formatting

--- HTMLCSSJS_Environment/areaChart.js
+++ HTMLCSSJS_Environment/areaChart.js
@@ -1,23 +1,23 @@
 new Chart(document.getElementById("myChart"), {
     type: 'line',
     data: {
-        labels: ['June', 'July', 'August'],
+        labels: ['June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
-            data: [100, 200, 500],
+            data: [100, 200, 500, 200, 100, 300, 400],
             label: "Account A",
-            borderColor: "#81b6cc",
+            borderColor: "#6b6b6b",
             backgroundColor: "#81b6cc",
             fill: true
         }, {
-            data: [200, 400, 100],
+            data: [200, 400, 100, 200, 300, 400, 100],
             label: "Account B",
-            borderColor: "#81cc9e",
+            borderColor: "#6b6b6b",
             backgroundColor: "#81cc9e",
             fill: true
         }, {
-            data: [300, 200, 100],
+            data: [300, 200, 100, 200, 300, 100, 500],
             label: "Account C",
-            borderColor: "#9081cc",
+            borderColor: "#cccccc",
             backgroundColor: "#9081cc",
             fill: true
         }
@@ -43,10 +43,16 @@ new Chart(document.getElementById("myChart"), {
                 }
             },
             y: {
+                beginAtZero: true,
                 stacked: true,
                 title: {
                     display: true,
-                    text: 'Value'
+                    text: 'Total Money'
+                },
+                ticks: {
+                    callback: function (value, index, values) {
+                        return '£' + value;
+                    }
                 }
             }
         }
