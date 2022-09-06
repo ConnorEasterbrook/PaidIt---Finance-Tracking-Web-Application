ca07ac7a6b2cbd624d819aa55fb634ea0bd019d9
Connor <connoreaster@gmail.com>
2022-09-06 14:22:02 +0100

    Styling

--- HTMLCSSJS_Environment/inputMenu.js
+++ HTMLCSSJS_Environment/inputMenu.js
@@ -4,18 +4,31 @@ var span = document.getElementsByClassName("close")[0];
 
 button.onclick = function () 
 {
-    inputMenu.style.display = "block";
+    // inputMenu.style.display = "block";
+
+    inputMenu.style.visibility = "visible";
+    inputMenu.style.transition = 'all 0.2s';
+    inputMenu.style.opacity = '1';
 }
 
 span.onclick = function ()
 {
-    inputMenu.style.display = "none";
+    // inputMenu.style.display = "none";
+
+    inputMenu.style.visibility = "hidden";
+    inputMenu.style.transition = 'all 0.2s';
+    inputMenu.style.opacity = '0';
 }
 
 window.onclick = function (event)
 {
     if (event.target == inputMenu)
     {
-        inputMenu.style.display = "none";
+        // inputMenu.style.display = "none";
+
+        inputMenu.style.visibility = "hidden";
+        inputMenu.style.transition = 'all 0.2s';
+        inputMenu.style.opacity = '0';
     }
 }
+
