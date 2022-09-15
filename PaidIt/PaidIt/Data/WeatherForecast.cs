d946fdfa238390174e8ee3364c97b5b74308e2ce
Connor <connoreaster@gmail.com>
2022-09-15 17:46:42 +0100

    Input Tutorial

--- PaidIt/PaidIt/Data/WeatherForecast.cs
+++ PaidIt/PaidIt/Data/WeatherForecast.cs
@@ -1,12 +1,13 @@
+using SQLite;
 namespace PaidIt.Data;
 
 public class WeatherForecast
 {
-	public DateTime Date { get; set; }
-
-	public int TemperatureC { get; set; }
-
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-	public string Summary { get; set; }
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF { get; set; }
+    [MaxLength(4000)]
+    public string Summary { get; set; }
 }
